b1f162956c65aad5ca2906761ed6a8e43e8d4cca
sunnyfu_i <sunnyfu_i@didiglobal.com>
2022-03-08 22:59:35 +0800

    then函数

--- MyPromise.js
+++ MyPromise.js
@@ -1,6 +1,21 @@
 const PENDING = 'pending'
 const FULFILLED = 'fulfilled'
 const REJECTED = 'rejected'
+
+function runMicroTask(callback) {
+    if (process && process.nextTick) {
+        process.nextTick(callback)
+    } else if (MutationObserver) {
+        const p = document.createElement('p')
+        const observer = new MutationObserver(callback)
+        observer.observe(p, {
+            childList: true
+        })
+        p.innerHTML = '123'
+    } else {
+        setTimeout(callback, 0);
+    }
+}
 class MyPromise {
     constructor(executor) {
         this._state = PENDING;
@@ -10,6 +25,12 @@ class MyPromise {
         } catch (error) {
             this._reject(error)
         }
+    }
+    then(onFulfulled, onRejected) {
+        return new MyPromise((resolve, reject) => {
+            // 1. 成功或者失败都会做的函数
+            // 2. 微队列
+        })
     }
     _changeState(newState, value) {
         if (this._state !== PENDING) return;
